0a25113775047e237213a2e54422027cc203bb47
anubhavRaj27 <anubhav.27.april@gmail.com>
2023-11-04 19:35:14 +0530

    dasboard

--- client/src/components/Uicomponents/Navbar.jsx
+++ client/src/components/Uicomponents/Navbar.jsx
@@ -10,15 +10,15 @@ export const Navbar = () => {
       </Link>
       <div className="content-outer">
       <div className='contents'>
-      <Link to="/" style={{ textDecoration: 'none',color: '#0F0F0F',fontFamily: 'Segoe UI', fontStyle: 'bold', fontSize: 'large' }}>
-        <div className="dashboard">Dashboard<br/></div>
-      </Link>
-      <Link style={{ textDecoration: 'none',color: '#0F0F0F',fontFamily: 'Segoe UI', fontStyle: 'bold', fontSize: 'large' }}>
-        <div className="courses">Courses<br/></div>
-      </Link>
-      <Link to="/explore" style={{ textDecoration: 'none',color: '#0F0F0F',fontFamily: 'Segoe UI', fontStyle: 'bold', fontSize: 'large' }}>
-        <div className="explore">Explore<br/></div>
-      </Link>
+        <Link to="/" style={{ textDecoration: 'none',color: '#0F0F0F',fontFamily: 'Segoe UI', fontStyle: 'bold', fontSize: 'large' }}>
+          <div className="dashboard">Dashboard<br/></div>
+        </Link>
+        <Link style={{ textDecoration: 'none',color: '#0F0F0F',fontFamily: 'Segoe UI', fontStyle: 'bold', fontSize: 'large' }}>
+          <div className="courses">Courses<br/></div>
+        </Link>
+        <Link to="/explore" style={{ textDecoration: 'none',color: '#0F0F0F',fontFamily: 'Segoe UI', fontStyle: 'bold', fontSize: 'large' }}>
+          <div className="explore">Explore<br/></div>
+        </Link>
       </div>
       </div>
     </div>
