3a5de82218a21928b1e80104bb0d1d6cdaa99697
Harshini <aggarwalharshini@gmail.com>
2023-11-04 19:52:29 +0530

    dashboard updated

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import {Navbar} from "./Uicomponents/Navbar"
 import './Dashboard.css'
 const Dashboard = () => {
   return (
-    <div className='dashboardPage'>
+    <div className='dashboard2'>
       <Navbar />
     </div>
 
