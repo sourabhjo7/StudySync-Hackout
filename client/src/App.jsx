06e89ee7fe115d0ac6cf05dd2590244aca1a48ad
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2023-11-05 00:17:53 +0530

    working for player functionality init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Explore from "./components/Explore.jsx/Explore";
 import Courses from "./components/courses/Courses";
 import ProtectedRoute from "./components/ProtectedRoute";
 import axios from "axios";
+import Youtubeplayer from "./components/youtubeplayer/Youtubeplayer";
 
 const BaseServerURL = "http://localhost:3000";
 function App() {
@@ -49,6 +50,14 @@ function App() {
             element={
               <ProtectedRoute isLoggedIn={isLoggedIn}>
                 <Courses />
+              </ProtectedRoute>
+            }
+          />
+            <Route
+            path="/player"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}>
+               <Youtubeplayer/>
               </ProtectedRoute>
             }
           />
