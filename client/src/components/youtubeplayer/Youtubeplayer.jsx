f5fb7a833fffcd970bcec3bca8837e151dafae31
Harshini <aggarwalharshini@gmail.com>
2023-11-05 01:11:11 +0530

    videoplayer page

--- client/src/components/youtubeplayer/Youtubeplayer.jsx
+++ client/src/components/youtubeplayer/Youtubeplayer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import "./Youtubeplayer.css"
 const Youtubeplayer = () => {
   return (
-    <div>Youtubeplayer</div>
+    <div className='playerbox'>
+      <div className="left-box">
+        <div className="top-left-box">Top (60%)</div>
+        <div className="bottom-left-box">Bottom (40%)</div>
+      </div>
+      <div className="right-box">Right Box (30%)</div>
+    </div>
   )
 }
 
