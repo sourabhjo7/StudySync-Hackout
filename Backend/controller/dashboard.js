4948ae07ade9cb58372ab7902936ca5562d374d1
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2023-11-05 01:55:52 +0530

    creating getting api data for video with youtube data api

--- Backend/controller/dashboard.js
+++ Backend/controller/dashboard.js
@@ -73,3 +73,4 @@ exports.getCoursesByUser = async (req, res) => {
     });
   }
 };
+
---
+++ client/src/getvideobyplaylist.js
@@ -0,0 +1,51 @@
+const API_KEY = "AIzaSyCqHotHqFvV211h6ev84Rw3egDf8SNWlGc";
+const _URL = "https://www.googleapis.com/youtube/v3/playlistItems";
+var result = [];
+var nextPageToken;
+
+const getVideos = async (playlistID) => {
+  const options = {
+    part: "snippet",
+    key: API_KEY,
+    maxResults: 500,
+    playlistId: playlistID,
+  };
+  const url = getFetchUrl(options);
+
+  let data = await (await fetch(url)).json();
+  const totalVideos = await data.pageInfo.totalResults;
+  result = data.items;
+  nextPageToken = data.nextPageToken;
+
+  while (true) {
+    if (result.length === totalVideos) {
+      break;
+    }
+    const returnedArr = await getMoreVideos(options, nextPageToken);
+    if (returnedArr.length === result.length) {
+      break;
+    } else {
+      result = returnedArr;
+    }
+  }
+  return result;
+};
+
+const getMoreVideos = async (options, pageToken) => {
+  const newOptions = { ...options, pageToken };
+  const url = getFetchUrl(newOptions);
+  const data = await (await fetch(url)).json();
+  nextPageToken = data.nextPageToken;
+  return [...result, ...data.items];
+};
+
+const getFetchUrl = (options) => {
+  var url = new URL(_URL),
+    params = options;
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+  return url;
+};
+
+export default getVideos;
