308f2246f8169d17411a0035446d4144fd6c5a25
anubhavRaj27 <anubhav.27.april@gmail.com>
2023-11-05 03:12:15 +0530

    courses page card display

---
+++ client/src/components/Uicomponents/CardCourse.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+
+
+const CardCourse = ({result,buttonData}) => {
+  return (
+    <Card sx={{ width: '400px'  }} style={{ marginBottom: "20px" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="{result?.snippet?.thumbnails?.high?.height}"
+          width="{result?.snippet?.thumbnails?.high?.width}"
+          image={result?.thumbnail}
+        />
+        <CardContent sx={{ backgroundColor: "#F5E8C7" }}>
+          <Typography gutterBottom variant="h5" component="div">
+            {result?.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {result?.description}
+          </Typography>
+        </CardContent>
+        <CardActions
+          sx={{
+            backgroundColor: "#F5E8C7",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Button
+            size="small"
+            sx={{ backgroundColor: "#FFFFFF", color: "black" }}
+          >
+            {buttonData}{" "}
+          </Button>
+          <Typography variant="h6" color="text.secondary">
+            {result?.channelTitle}
+          </Typography>
+        </CardActions>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default CardCourse
