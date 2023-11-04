812c5eb08b13d247f78b67b420b2e9b3b996118a
Harshini <aggarwalharshini@gmail.com>
2023-11-04 20:25:39 +0530

    dashboard page updated

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -5,6 +5,23 @@ const Dashboard = () => {
   return (
     <div className='dashboard2'>
       <Navbar />
+      <div className="container">
+      <div className="box">
+        <div className="text">
+            Enrolled Courses
+        </div>
+      </div>
+      <div className="box">
+        <div className='text'>
+          Progress
+        </div>
+      </div>
+      <div className="box">
+        <div className='text'>
+          Profile
+        </div>
+      </div>
+    </div>
     </div>
 
   )
