910dd8ebcde11c0167afb9b80438793c1e2bd439
anubhavRaj27 <anubhav.27.april@gmail.com>
2023-11-05 07:14:20 +0530

    videolist component

--- client/src/components/Uicomponents/VideoList.jsx
+++ client/src/components/Uicomponents/VideoList.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import { useState } from 'react';
 
-const VideoList = () => {
+const VideoList = ({videos}) => {
   const [checked, setChecked] = useState([0]);
 
   const handleToggle = (value) => () => {
@@ -24,27 +23,23 @@ const VideoList = () => {
   };
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
-
+      {videos?.map((value) => {
         return (
           <ListItem
-            key={value}
+            key={value.etag}
             disablePadding
           >
-            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItemButton>
-          </ListItem>
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                disableRipple
+                onClick={handleToggle(value)}
+              />
+            </ListItemIcon>
+            <ListItemText id={value.etag} primary={value.snippet.title} />
+        </ListItem>
         );
       })}
     </List>
